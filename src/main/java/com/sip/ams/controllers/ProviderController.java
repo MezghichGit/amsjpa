afb4a7f8734cb94466cee4c2681be8f4aac83514
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-10-10 13:00:26 +0100

    Bare de recherche HQL 2 méthodes

--- src/main/java/com/sip/ams/controllers/ProviderController.java
+++ src/main/java/com/sip/ams/controllers/ProviderController.java
@@ -1,6 +1,7 @@
 package com.sip.ams.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sip.ams.entities.Article;
@@ -95,6 +97,21 @@ public class ProviderController {
 		model.addAttribute("articles", articles);
 		model.addAttribute("provider", provider);
 		return "provider/showProvider";
+	}
+	
+	@PostMapping("search")
+	//@ResponseBody
+	public String findProviderByName(@RequestParam("nom")String name,Model model) {
+		//List<Provider> lp = providerRepository.ListProviderName(name);
+		List<Provider> lp = providerRepository.findProviderByNameLike(name);
+		
+		if (lp.size() == 0)
+			lp = null;
+		 System.out.println(lp);
+		// System.out.println(lp.size());
+		model.addAttribute("providers", lp);
+		return "provider/listProviders";
+		//return "Vous avez taper : "+ name;
 	}
 
 }
--- src/main/java/com/sip/ams/repositories/ProviderRepository.java
+++ src/main/java/com/sip/ams/repositories/ProviderRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import com.sip.ams.entities.Article;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.sip.ams.entities.Provider;
 @Repository
@@ -12,5 +13,12 @@ public interface ProviderRepository extends CrudRepository<Provider, Long> {
 	
 	@Query("FROM Article a WHERE a.provider.id = ?1")
 	List<Article> findArticlesByProvider(long id);
+	
+	
+	@Query("FROM Provider p WHERE p.name like %?1%")
+	List<Provider> ListProviderName(String nom);
+	
+	@Query("select p from Provider p where p.name like %:name%")
+	List<Provider> findProviderByNameLike(@Param("name") String name);
 
 }
