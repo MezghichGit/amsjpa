6014a0d3f5bcd77ae719783679092752e6946d92
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-10-10 11:17:37 +0100

    version finale pour Tomcat

--- src/main/java/com/sip/ams/AmsMvcdataApplication.java
+++ src/main/java/com/sip/ams/AmsMvcdataApplication.java
@@ -2,12 +2,13 @@ package com.sip.ams;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import com.sip.ams.controllers.ArticleController;
 
 import java.io.File;
 @SpringBootApplication
-public class AmsMvcdataApplication {
+public class AmsMvcdataApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		new File(ArticleController.uploadDirectory).mkdir();
